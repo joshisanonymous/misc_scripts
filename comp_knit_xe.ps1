21b73f0a74fc242daf4e2b94aa4ac19b2b20b5a4
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2019-07-12 15:27:54 -0400

    Updated bracketing style & made all done msg ever so slightly flashier

--- comp_knit_xe.ps1
+++ comp_knit_xe.ps1
@@ -13,46 +13,40 @@
 $file = $args[0]
 
 # Check if the document needs knitting and which file extension
-if (test-path -path "$file.Rnw")
-  {
+if (test-path -path "$file.Rnw") {
   rscript -e "library(knitr); knit('$file.Rnw')"
-  }
-elseif (test-path -path "$file.Rtex")
-  {
+} elseif (test-path -path "$file.Rtex") {
   rscript -e "library(knitr); knit('$file.Rtex')"
-  }
+}
 
 # Perform typical compilation, checking if natbib or biblatex were used along
 # the way
 xelatex "$file.tex"
-if ((get-content "$file.tex") -match "natbib")
-  {
+if ((get-content "$file.tex") -match "natbib") {
   bibtex "$file.aux"
   xelatex "$file.tex"
-  }
-elseif ((get-content "$file.tex") -match "biblatex")
-  {
+} elseif ((get-content "$file.tex") -match "biblatex") {
   biber "$file"
   xelatex "$file.tex"
-  }
+}
 xelatex "$file.tex"
-if ((get-content "$file.tex") -match "{exam}")
-  {
+if ((get-content "$file.tex") -match "{exam}") {
   xelatex "$file.tex"
-  }
+}
 sumatrapdf "$file.pdf"
 
 # Ask the user if they want to delete files then act accordingly
 $delete = read-host "Do you want to delete generated intermediary files? (y/n)"
 
 # Cleanup
-if (($delete -match "[yY]") -and (test-path -path "$file.Rnw"))
-  {
+if (($delete -match "[yY]") -and (test-path -path "$file.Rnw")) {
   remove-item "$file.tex", "$file.out", "$file.blg", "$file.bbl", "$file.aux", "$file.log", "$file.nav", "$file.snm", "$file.toc", "$file.bcf", "$file.run.xml"
-  }
-elseif ($delete -match "[yY]")
-  {
+} elseif ($delete -match "[yY]") {
   remove-item "$file.out", "$file.blg", "$file.bbl", "$file.aux", "$file.log", "$file.nav", "$file.snm", "$file.toc", "$file.bcf", "$file.run.xml"
-  }
+}
 
-write-host "All done!"
+write-host @"
+ -----------
+| All done! |
+ -----------
+"@
