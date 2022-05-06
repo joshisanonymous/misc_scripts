58dab57e91f6aded1722a16d701138d6256dfc65
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2022-05-06 13:26:38 -0400

    Now prints email addresses of all current students

--- slack_invite/invite_new_students.py
+++ slack_invite/invite_new_students.py
@@ -10,12 +10,16 @@ baseUrl = "/directory/people/"
 # Derived variables
 directoryCode = requests.get(webSite + peopleDirectory)
 directoryHtml = BeautifulSoup(directoryCode.text, "html.parser")
+# slackMembers = # get the emails from the current workspace members
 
 # Get all URLs that are for student pages
-students = []
 for link in directoryHtml.find_all("a"):
     url = link.get("href")
     if url.startswith(baseUrl):
-        students.append(webSite + url)
-
-students
+        # print(url)
+        studentCode = requests.get(webSite + url)
+        studentHtml = BeautifulSoup(studentCode.text, "html.parser")
+        for linkStudent in studentHtml.find_all("a"):
+            urlStudent = linkStudent.get("href")
+            if urlStudent.startswith("mailto:") and not urlStudent.endswith("linguistics@uga.edu"):
+                print(urlStudent.strip("mailto:"))
